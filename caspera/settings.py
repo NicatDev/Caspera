bfd7551a5d18b7c0dfca660370127737f8248d3e
NicatDev <nicat254memmedov@gmail.com>
2024-01-06 18:10:34 +0400

    22

--- caspera/settings.py
+++ caspera/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vt@akc1@n*&dl@c_9ls+hxf+&ny4yd1p4s-i4r0908!lff4%ep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
