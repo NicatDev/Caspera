ce022ed0322d6cdb7301fff434328c21e2cbccb0
NicatDev <nicat254memmedov@gmail.com>
2024-01-06 18:03:15 +0400

    22

--- caspera/urls.py
+++ caspera/urls.py
@@ -19,10 +19,31 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from mainapp.sitemap import BlogSiteMap, PortfolioCategorySitemap,  ServiceSitemap, StaticSitemap,TagSiteMap
+from django.views.generic import TemplateView
+
+sitemaps = {
+    'blog_sitemap': BlogSiteMap,
+    'service_sitemap': ServiceSitemap,
+    'tag_category': TagSiteMap,
+    'portfolio_category': PortfolioCategorySitemap,
+    'static_sitemap': StaticSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('mainapp.urls'))
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include("mainapp.urls")),
+    path(
+        'sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'
+    ),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
+    prefix_default_language=False
+)
+
 urlpatterns += static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
